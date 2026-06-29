9453c0e58bf4f81b9e3dd91f2aae9f3cc56f2eb0
omar44s <lymr90280@gmail.com>
2026-06-29 16:12:48 +0300

    Fix duplicate welcome

--- index.js
+++ index.js
@@ -19,22 +19,20 @@ client.once('clientReady', () => {
 
 client.on('guildMemberAdd', async (member) => {
 
-    // آيدي روم الترحيب
     const channel = member.guild.channels.cache.get('1520694287476588627');
 
     if (!channel) return;
 
     try {
 
-        // إنشاء اللوحة
         const canvas = Canvas.createCanvas(1000, 667);
         const ctx = canvas.getContext('2d');
 
-        // تحميل الخلفية
+        // الخلفية
         const background = await Canvas.loadImage('./welcome.png');
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-        // تحميل صورة العضو
+        // صورة العضو
         const avatar = await Canvas.loadImage(
             member.user.displayAvatarURL({
                 extension: 'png',
@@ -42,31 +40,28 @@ client.on('guildMemberAdd', async (member) => {
             })
         );
 
-        // وضع صورة العضو داخل الدائرة
         ctx.save();
 
+        // مكان الدائرة السوداء بالضبط
         ctx.beginPath();
-        ctx.arc(225, 295, 90, 0, Math.PI * 2, true);
+        ctx.arc(260, 315, 115, 0, Math.PI * 2, true);
         ctx.closePath();
         ctx.clip();
 
-        ctx.drawImage(avatar, 135, 205, 180, 180);
+        // مكان صورة العضو داخل الدائرة
+        ctx.drawImage(avatar, 145, 200, 230, 230);
 
         ctx.restore();
 
-        // إنشاء الصورة
         const attachment = new AttachmentBuilder(
             canvas.toBuffer('image/png'),
             { name: 'welcome.png' }
         );
 
-        // إرسال الصورة ورسالة الترحيب
         await channel.send({
-            content: `##  أهلاً بك ${member}`,
+            content: `#  أهلاً بك ${member}`,
             files: [attachment]
         });
-
-        console.log(`Welcome sent to ${member.user.tag}`);
 
     } catch (error) {
         console.log(error);
