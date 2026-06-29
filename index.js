2d1851e3e898817999496f2f9032e5bb4fc3ed66
omar44s <lymr90280@gmail.com>
2026-06-29 15:58:03 +0300

    Fix avatar size

--- index.js
+++ index.js
@@ -19,21 +19,22 @@ client.once('clientReady', () => {
 
 client.on('guildMemberAdd', async (member) => {
 
+    // آيدي روم الترحيب
     const channel = member.guild.channels.cache.get('1520694287476588627');
 
     if (!channel) return;
 
     try {
 
-        // نفس نسبة أبعاد الصورة الأصلية
+        // حجم الصورة
         const canvas = Canvas.createCanvas(768, 512);
         const ctx = canvas.getContext('2d');
 
-        // الخلفية
+        // تحميل الخلفية
         const background = await Canvas.loadImage('./welcome.png');
-        ctx.drawImage(background, 0, 0, 768, 512);
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-        // صورة العضو
+        // تحميل صورة العضو
         const avatar = await Canvas.loadImage(
             member.user.displayAvatarURL({
                 extension: 'png',
@@ -41,27 +42,31 @@ client.on('guildMemberAdd', async (member) => {
             })
         );
 
-        // مكان صورة العضو داخل الدائرة
+        // وضع صورة العضو داخل الدائرة
         ctx.save();
 
         ctx.beginPath();
-        ctx.arc(235, 255, 100, 0, Math.PI * 2, true);
+        ctx.arc(220, 250, 80, 0, Math.PI * 2, true);
         ctx.closePath();
         ctx.clip();
 
-        ctx.drawImage(avatar, 135, 155, 200, 200);
+        ctx.drawImage(avatar, 140, 170, 160, 160);
 
         ctx.restore();
 
+        // إنشاء الصورة
         const attachment = new AttachmentBuilder(
             canvas.toBuffer('image/png'),
             { name: 'welcome.png' }
         );
 
+        // إرسال الرسالة
         await channel.send({
             content: `أهلاً بك ${member}`,
             files: [attachment]
         });
+
+        console.log(`Welcome sent to ${member.user.tag}`);
 
     } catch (error) {
         console.log(error);
